4c0c0d1d6aa1cb779528837050e6135b3451bad9
Предеин Никита <ne-kit@outlook.com>
2020-11-01 00:40:30 +0500

    Rel.1.2

---
+++ build/precache-manifest.c5a7236a71a81bef743f87e34ad4c833.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d38b5b9393b1f164a7a0",
+    "url": "/static/js/main.d38b5b93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "e54f28ea85ad68994a3e",
+    "url": "/static/css/2.c524794f.chunk.css"
+  },
+  {
+    "revision": "e54f28ea85ad68994a3e",
+    "url": "/static/js/2.e54f28ea.chunk.js"
+  },
+  {
+    "revision": "1d79f9f417076249068beb8237cc4604",
+    "url": "/index.html"
+  }
+];
